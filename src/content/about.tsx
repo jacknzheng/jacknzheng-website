4fad9c9c81b19e19b5efe518238012831c6165ca
jackzengh <jackzengh2004@gmail.com>
2026-09-11 15:54:23 -0700

    Update about bio and contact information

--- src/content/about.tsx
+++ src/content/about.tsx
@@ -8,10 +8,26 @@ export default function About() {
       </p>
 
       <p>
-        Before this I was studying law in Melbourne, Australia, which I left
-        after sophomore, to join Superpower. I studied political philosophy and
-        economics, and I previously was a competitive national debater.
+        Before this, I was studying law on a full scholarship in Melbourne,
+        Australia. I also studied political philosophy and economics, and worked
+        in Australian parliament under Keith Wolahan MP. I previously was also a
+        competitive national debater, in the Unimelb firsts team.
       </p>
+
+      <p>
+        After sophomore, I left university to join Superpower. I helped scale
+        paid media from zero to our largest acquisition channel, through to
+        Series B. Our team was mostly 2 full-timers. I also worked on our web
+        design, sold peptides and prescriptions, and did medical evals.
+      </p>
+
+      <p>
+        Now, I am doing pre-training research, advised by a good friend Jeffrey
+        Cheng, at South Park Commons. I also run a debate club on Friday nights
+        where we discuss AI safety, geopolitics and economic policy.
+      </p>
+
+      <p>You can reach me at jackzengh2004[at]gmail.com</p>
     </>
   );
 }
