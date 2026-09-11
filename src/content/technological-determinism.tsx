fe2a6e89709644754b0ed737433e195d7fbcbd2c
jackzengh <jackzengh2004@gmail.com>
2026-09-10 18:10:26 -0700

    edits

--- src/content/technological-determinism.tsx
+++ src/content/technological-determinism.tsx
@@ -399,7 +399,7 @@ export default function TechnologicalDeterminism() {
         }
         <a href={"https://www.sandia.gov/about/history/1960s/"}>{"[19]"}</a>
         {
-          " Separate safeguards, including environmental sensing devices that check conditions match a predetermined launch sequence, and accident-safety switches, were also designed to prevent unintentional detonation. In 1961, a B-52 bomber carrying two Mark 39 hydrogen bombs broke apart over Goldsboro, North Carolina. One bomb progressed through several stages of its arming sequence, but one switch remaining in the safe position preventing catastrophe. While PALs, a preventative technology developed to control the capabilities of nuclear weapons, are certainly an outlier in some respect - they are not alone. Emergency core cooling mechanisms were developed alongside in nuclear reactors and biological safety cabinets for epidemiology were also instantiated to control the existential risks associated with new technologies."
+          " Separate safeguards, including environmental sensing devices that check conditions match a predetermined launch sequence, and accident-safety switches, were also designed to prevent unintentional detonation. In 1961, a B-52 bomber carrying two Mark 39 hydrogen bombs broke apart over Goldsboro, North Carolina. One bomb progressed through several stages of its arming sequence, but one switch remaining in the safe position preventing catastrophe. While PALs, a preventative technology developed to control the capabilities of nuclear weapons, are certainly an outlier in some respect - they are not alone. Emergency core cooling mechanisms were developed alongside nuclear reactors as were biological safety cabinets for laboratory research, to control the existential risks associated with new technologies."
         }
       </p>
       <h2 id={"ai-safety-needs-policy-intervention"}>
@@ -423,13 +423,13 @@ export default function TechnologicalDeterminism() {
         }
         <a href={"https://arxiv.org/abs/2406.11717"}>{"[23]"}</a>
         {
-          " While AI safety is not underfunded, it certainly is, relative to the capital dedicated to push frontier capabilities. Safety at most frontier labs are ad-addendum to the core training process due to a lack of financial incentive to push alignment as far as capabilities "
+          " While AI safety is not underfunded in absolute terms, it certainly is, relative to the capital dedicated to push frontier capabilities. Safety at most frontier labs are ad-addendum to the core training process due to a lack of financial incentive to push alignment as far as capabilities "
         }
         <a href={"https://techcrunch.com/2026/02/14/is-safety-is-dead-at-xai/"}>
           {"[24]"}
         </a>
         {
-          ". Highly capable Chinese open weight models are released on the open internet. And it is not unreasonable to expect the Hugging Face hack, and wiki message boards are just the beginning of us discovering emergent multi-agent behavior. "
+          ". Highly capable Chinese open weight models are released on the open internet. Multi-agent behavior shows swarms of models can pursue complex tasks via unaligned means, through cooperation. "
         }
         <a href={"https://collusion.wiki/"}>{"[25]"}</a>{" "}
         <a
@@ -440,7 +440,7 @@ export default function TechnologicalDeterminism() {
           {"[26]"}
         </a>
         {
-          " The negative externalities of AI may be difficult for a free market to internalize, and so as with PALs, it is my contention that command and control technologies will have to be a public-led effort and likely an initiative akin to the Manhattan project, as model capabilities continue to outpace our understanding of models themselves."
+          " As with nuclear weapons, deceleration attempts - such as nuclear non-proliferation, have scarcely prevented actors from building new capabilities. Pursuing strong command and control mechanisms, however, as with PALs, seem to be one of the few ways to mitigate the negative externalities of potentially catastrophic technologies. PALs have ensured that nuclear weapons cannot be detonated by rogue actors and its capabilities cannot be accidentally activated or used discreetly. Our PAL designs were subsequentialy shared with the Soviet Union and France, and have become a nuclear standard. This effort was intentionally led by the government and mandated by the president, not the predetermined product of prior technological developments. Our future, and that of alignment, will be steered by the willpower of people, researchers and policymakers, not the amorphous force of technology. "
         }
       </p>
       <h2 id={"conclusion"}>{"Conclusion"}</h2>
